734c8daf1f8085417a37a0976d8216747c55d6bc
Derick Bailey <derickbailey@gmail.com>
2017-05-02 13:24:06 -0500

    remove old build folder

--- build/objectRest.js
+++
@@ -1,9 +0,0 @@
-function doStuff(){
-  console.log("object rest properties {x, y, ...z}");
-
-  let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 }; 
-
-  console.log("Result:", x, y, z);
-}
-
-doStuff();
